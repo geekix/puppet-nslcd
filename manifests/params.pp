026b1a310c9a874fb496dbe43719b663806ceeb8
matt.cover <matt.cover@highwinds.com>
2015-10-30 12:35:51 -0700

    Add params and tests for CentOS and RedHat.

--- manifests/params.pp
+++ manifests/params.pp
@@ -27,11 +27,21 @@ class nslcd::params {
       $config        = $default_config
       $package_name  = $default_package_name
       $service_name  = $default_service_name
-      $uid          = 'nslcd'
-      $gid         = 'nslcd'
-      $config_user         = 'root'
-      $config_group        = 'nslcd'
-      $config_mode         = '0640'
+      $uid           = 'nslcd'
+      $gid           = 'nslcd'
+      $config_user   = 'root'
+      $config_group  = 'nslcd'
+      $config_mode   = '0640'
+    }
+    RedHat: {
+      $config        = $default_config
+      $package_name  = 'nss-pam-ldapd'
+      $service_name  = $default_service_name
+      $uid           = 'nslcd'
+      $gid           = 'ldap'
+      $config_user   = 'root'
+      $config_group  = 'root'
+      $config_mode   = '0600'
     }
     default: {
       fail("The ${module_name} module is not supported on an ${::osfamily} based system.")
