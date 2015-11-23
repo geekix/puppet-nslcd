6bca0ed5d8db8f48aba93b7fd1daa7608b746cbc
t0xicCode <xavier@openconcept.ca>
2015-11-23 18:42:05 -0500

    Updated case to support puppet 4

--- manifests/params.pp
+++ manifests/params.pp
@@ -23,18 +23,18 @@ class nslcd::params {
   $default_service_name = 'nslcd'
 
   case $::osfamily {
-    Debian: {
-      $config        = $default_config
-      $package_name  = $default_package_name
-      $service_name  = $default_service_name
+    'debian': {
+      $config       = $default_config
+      $package_name = $default_package_name
+      $service_name = $default_service_name
       $uid          = 'nslcd'
-      $gid         = 'nslcd'
-      $config_user         = 'root'
-      $config_group        = 'nslcd'
-      $config_mode         = '0640'
+      $gid          = 'nslcd'
+      $config_user  = 'root'
+      $config_group = 'nslcd'
+      $config_mode  = '0640'
     }
     default: {
-      fail("The ${module_name} module is not supported on an ${::osfamily} based system.")
+      fail("the ${module_name} module is not supported on an ${::osfamily} based system.")
     }
   }
 
