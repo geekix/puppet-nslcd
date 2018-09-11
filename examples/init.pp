48d94d5782c08a9385963305de3e30a0305601bf
hdep <hugo.deprez@protonmail.com>
2018-09-11 22:28:39 +0200

    fixing indentation

--- examples/init.pp
+++ examples/init.pp
@@ -10,26 +10,26 @@
 # http://docs.puppetlabs.com/guides/tests_smoke.html
 #
 class { 'nslcd':
-  ldap_uris => ['ldaps://ldap1.domain.local', 'ldaps://ldap2.domain.local'],
-  ldap_binddn => 'ldap-nslcd@domain.local',
-  ldap_bindpw => 'changeMe',
-  ldap_tls_reqcert =>  'demand',
-  ldap_ssl  => 'on',
-  ldap_search_base => 'dc=domain,dc=local',
+  ldap_uris           => ['ldaps://ldap1.domain.local', 'ldaps://ldap2.domain.local'],
+  ldap_binddn         => 'ldap-nslcd@domain.local',
+  ldap_bindpw         => 'changeMe',
+  ldap_tls_reqcert    =>  'demand',
+  ldap_ssl            => 'on',
+  ldap_search_base    => 'dc=domain,dc=local',
   ldap_tls_cacertfile => '/usr/share/ca-certificates/DOMAIN/DOMAIN_ROOT.pem',
-  config_options => {
+  config_options      => {
     'referrals'        => 'no',
     'pam_authz_search' => '(&(memberof=CN=sysop,OU=Groups,DC=domain,DC=local)(objectClass=user)(sAMAccountName=$username))',
     'nss_min_uid'      => '2000',
     'idle_timelimit'   => '120',
     'threads'          => '10'
   },
-  ldap_filters => {
+  ldap_filters        => {
     'passwd' => '(&(objectClass=user)(!(objectClass=computer))(uidNumber=*)(HomeDirectory=*))',
     'shadow' => '(&(objectClass=user)(!(objectClass=computer))(uidNumber=*)(HomeDirectory=*))',
     'group'  => '(&(objectClass=group)(gidNumber=*))',
   },
-  ldap_maps => {
+  ldap_maps           => {
     '1' => 'passwd uid sAMAccountName',
     '2' => 'passwd gecos displayName',
     '3' => 'shadow uid  sAMAccountName',
